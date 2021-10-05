cf4985ca4f048d32d2bae19fd65cb6545819f85f
trevorwebb <89754721+trevorwebb@users.noreply.github.com>
2021-10-04 21:49:06 -0400

    Add primary keys and references

--- working_draft.sql
+++ working_draft.sql
@@ -1,22 +1,27 @@
 Create database if not exists ref;
 use ref;
 
-create table if not exists Referee (
+create table if not exists REFREE (
  ref_ID varchar(8),
  first_name varchar(20),
  last_name varchar(20),
  date_of_birth date,
- ref_rate int
+ ref_rate int,
+ primary key (ref_ID)
 );
  
  
- create table if not exists Assignment (
+ create table if not exists ASSIGNMENT (
  assign_ID varchar(8),
- game_ID varchar(8), 
- ref_ID varchar(8),
- assign_status varchar(15));
+ game_ID varchar(8) references game(game_id), 
+ ref_ID varchar(8) references referee(ref_id),
+ assign_status varchar(15),
+ primary key (assign_id)
+ );
  
- Create table if not exists Game(
+ Create table if not exists GAME (
  game_ID varchar(8),
  game_level varchar(25),
- game_date date);
+ game_date date,
+ primary key (game_ID)
+ );
