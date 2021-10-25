bf87f873d62509b9f34bc0fd2ac32018e18286c9
trevorwebb <trevor.webb98@gmail.com>
2021-10-25 09:55:59 -0400

    Added test.php
    Test file for connection debugging

---
+++ app/public/api/test.php
@@ -0,0 +1,59 @@
+<?php
+
+$num = 2;
+$foo = $num . " be";
+$bar = "or not " .$num." be.";
+
+echo $foo . " " . $bar;
+
+echo "\n";
+
+echo $num * $num * $num;
+
+$arr = [1,1,2,3,5,8];
+
+$arr2 = [
+    "first" => "Tom",
+    "second" => "Bipin",
+    "best" => "DS"
+];
+
+if (true) {
+    echo "TRUE \n";
+} else {
+    echo "FALSE \n";
+}
+
+while (true) {
+    break;
+}
+
+echo "<ul>";
+foreach ($arr2 as $key => $val) {
+    echo "<li>".$key." is ".$val."</li>\n";
+}
+echo "<ul>";
+
+// $arr as json
+
+echo json_encode($arr);
+echo json_encode($arr2, JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR);
+
+// This is a comment
+# This is a comment
+/* This
+is a
+multi-line
+comment
+*/
+
+/***
+ * Variable naming
+ * 
+ * PHP and JS: camelCase
+ * Constants: UPPER_SNAKE_CASE
+ * 
+ * snake_case
+ * PascalCase (we'll use for class names)
+ * kebab-case
+ */
