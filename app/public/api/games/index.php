63dccd663bd268ce391fe82a91d0c81964f960ad
kpworrall <89401634+kpworrall@users.noreply.github.com>
2021-11-02 12:10:40 -0400

    games API

---
+++ app/public/api/games/index.php
@@ -0,0 +1,30 @@
+<?php
+require 'class/DbConnection.php';
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$sql = 'SELECT * FROM GAMES';
+$vars = [];
+
+if (isset($_GET['games'])) {
+  // This is an example of a parameterized query
+  $sql = 'SELECT * FROM GAMES WHERE id = ?';
+
+ 
+  $vars = [ $_GET['games'] ];
+}
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$games = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($referees, JSON_PRETTY_PRINT);
+
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
