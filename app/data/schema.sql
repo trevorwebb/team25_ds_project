a814698a7e9368c92bb86ed59a6e0fba5df49dc5
kpworrall <89401634+kpworrall@users.noreply.github.com>
2021-11-03 09:30:14 -0400

    schema update

--- app/data/schema.sql
+++ app/data/schema.sql
@@ -3,15 +3,15 @@ Create database if not exists ref;
 use ref;
 
 create table if not exists REFEREES (
- ref_ID varchar(8),
+ id varchar(8),
  first_name varchar(20) NOT NULL,
  last_name varchar(20) NOT NULL,
  date_of_birth date NOT NULL,
  ref_rating int,
- primary key (ref_ID)
+ primary key (id)
 );
 
- Create table if not exists GAME (
+ Create table if not exists GAMES (
  game_ID varchar(8),
  game_level varchar(25) NOT NULL,
  game_date date NOT NULL,
@@ -35,7 +35,7 @@ insert into REFEREES values
    ('002','Tom','Gregory','1995-04-23',87),
    ('003','Alan','Dennis','1988-06-15',79);
    
-insert into GAME values
+insert into GAMES values
    ('101','1','2021-12-23'),
    ('102','3','2022-01-15');
    
@@ -44,3 +44,5 @@ insert into ASSIGNMENT values
    ('2000','102','002','Head','Tentative'),
    ('2001','102','003','Assistant','Unassigned'),
    ('2002','102','001','Assistant','Tentative');
+
+
